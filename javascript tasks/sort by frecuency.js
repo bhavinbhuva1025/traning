85790a0c8ced9031aff3ec75e0baf94edb481e8e
Bhavin Bhuva <bhavinbhuva59@gmail.com>
2025-11-14 16:23:47 +0530

    Add files via upload

---
+++ javascript tasks/sort by frecuency.js
@@ -0,0 +1,49 @@
+
+// // "Find Missing Number – Array contains 1...n, one number missing, find it.:-"	given an array of numbers and give pass argunents from and to,
+// // it's find missing number by given argument from and to.
+
+// // Sort by Frequency – Sort array eles by frequency of occurrence.	:-"solve by using nested array and nested for loop of an numbers frequency array.
+// // 1) convert normal array to object with each ele frequency.
+// // 2) convert frequency object to nested array without using built in methods.
+// // 3) sort an nested array and use nested for loop to push the each ele in final result array."
+
+
+function sortByFrequency(arr) {
+
+    let frequencyMap = {};
+    for (let i = 0; i < arr.length; i++) {
+        let ele = arr[i];
+        if (frequencyMap[ele]) {
+            frequencyMap[ele]++;
+        } else {
+            frequencyMap[ele] = 1;
+        }
+    }           
+    let frequencyArray = [];
+    for (let key in frequencyMap) {
+        frequencyArray.push([parseInt(key), frequencyMap[key]]);
+    }
+    for (let i = 0; i < frequencyArray.length - 1; i++) {   
+        for (let j = i + 1; j < frequencyArray.length; j++) {
+            if (frequencyArray[i][1] > frequencyArray[j][1]) {
+                let temp = frequencyArray[i];
+                frequencyArray[i] = frequencyArray[j];
+                frequencyArray[j] = temp;
+            }   
+        }
+    }
+    let sortedResult = [];
+    for (let i = 0; i < frequencyArray.length; i++) {
+        let ele = frequencyArray[i][0];
+        let count = frequencyArray[i][1];
+        for (let j = 0; j < count; j++) {
+            sortedResult.push(ele);
+        }
+    }
+    return sortedResult;
+}
+
+let arr = [4, 6, 2, 6, 7, 4, 4, 2, 6, 8, 8, 8, 8];
+let sortedByFrequency = sortByFrequency(arr);
+console.log("Original Array:", arr);
+console.log("Sorted by Frequency:", sortedByFrequency);
