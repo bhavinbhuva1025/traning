2e3d32780c6a4b1d96ed6b296e9c591d82013a2e
Bhavin Bhuva <bhavinbhuva59@gmail.com>
2025-11-16 10:11:54 +0530

    Implement custom palindrome check function
    Refactor palindrome check to avoid built-in methods and use a for loop.

--- javascript tasks/palindrom.js
+++ javascript tasks/palindrom.js
@@ -1,6 +1,19 @@
-function palindrom(str){
-  let string=str.toLowerCase().replace(/[^a-z0-9]/g, '');
-  let reversed=string.split('').reverse().join('');
-  return string===reversed;
+
+
+// solve without using built in methods like .reverse() and build own logic, using for   loop and if else block.
+function isPalindrome(str) {
+    str = str.toLowerCase();  
+    const len = str.length;
+    for (let i = 0; i < len / 2; i++) {
+        if (str[i] !== str[len - 1 - i]) {
+            return false; 
+        }
+    }
+    return true; 
 }
-console.log(palindrom("123") );
+let string = "Racecar";
+if (isPalindrome(string)) {
+    console.log(`${string} is a palindrome.`);
+} else {
+    console.log(`${string} is not a palindrome.`);
+}
